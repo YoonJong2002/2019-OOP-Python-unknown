1062cc4c13127b231d3ce44981be8a9592db0368
jessica314jessica <jessica314@sasa.hs.kr>
2019-11-25 09:32:30 +0900

    랭킹 sort 구현 완료

--- class_of_player.py
+++ class_of_player.py
@@ -5,7 +5,6 @@ class Player:
         self.collected_money = 0
         self.time_spent = 0
 
-
     def did_you_die(self):
         self.life_left = self.life_left - 1
         if self.life_left == 0:
@@ -15,7 +14,3 @@ class Player:
 
     def you_collected(self, how_much_collected):
         self.collected_money = self.collected_money + how_much_collected
-
-    def life_left_update(self):
-        self.life_left -= 1
-
